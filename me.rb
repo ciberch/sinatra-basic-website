8628bf2e13cddf0d97973283746258fa5bf14ed7
ciberch <monica.keller@gmail.com>
2012-03-30 01:23:58 -0700

    Small ogp issues

--- me.rb
+++ me.rb
@@ -9,7 +9,7 @@ get "/" do
   @me = YAML.load_file("poco.yml")
   @me = Hashie::Mash.new(@me)
   @full_url = url_for("/", :full)
-  @description = "Who is #{@me.preferred_username || @me.display_name}"
+  @description = "Who is #{@me.preferred_username || @me.display_name} ?"
   @image = url_for(@me.photos.first.value, :full)
   @title = @me.display_name
   @appid = ENV['facebook_app_id']
