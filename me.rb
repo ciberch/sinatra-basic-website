f03e95d9d6510ae150d36f01f8c803a181243e48
Monica Wilkinson <monica@crushpath.com>
2015-08-26 22:25:00 -0700

    Example with chat snippet

--- me.rb
+++ me.rb
@@ -5,6 +5,8 @@ class CiberchApp < Sinatra::Base
   helpers Sinatra::UrlForHelper
 
   get "/" do
+    @chat_url = ENV['CHAT_URL'] || "http://jeff-test.crushpath.us/spots/2756/chat"
+    @chat_base_url = ENV['CHAT_BASE_URL'] || "crushpath-stream-test.herokuapp.com"
     @me = YAML.load_file("poco.yml")
     @me = Hashie::Mash.new(@me)
     @full_url = url_for("/", :full)
